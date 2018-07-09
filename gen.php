dfb3b4257cfeea4bcfdb346c85ec079a2c115908
Yanyong Kunok <yanyong.kunok@gmail.com>
2018-07-09 23:05:35 +0700

    gen fixed

--- gen.php
+++ gen.php
@@ -1,7 +1,7 @@
 <?
 $datee= date('Y-m-d H:i:s');
 echo "<hr>$datee";
-$file ='data.txt';
+$file ='raw.txt';
 $da=file_get_contents($file,true);
 $p = explode("\n", $da);
 for ($j=0;$j<=count($p);$j++){
@@ -15,9 +15,7 @@ for ($i=0;$i<=$j;$i++){
 //if (strlen($po[$i])>0) $up.=$po[$i]."\r\n";
 $jst="'";
 if ($i<$j-2) $jsend="',"; else $jsend="'";
-
 if (strlen($p[$i])>0) $daa=$p[$i];
-
 $daa=str_replace("\r","",$daa);
 $daa=str_replace("'","\'",$daa);
 $daa=str_replace('"','\"',$daa);
@@ -26,13 +24,13 @@ $ld=strlen($p[$i]);
 	if ($ld>0) $jnew.=$jst.$daa.$jsend;	
 }
 $jnew.="]";
-
+//echo $jnew;
 $var=" generate to js file .. $cpl #recs".PHP_EOL;
 $datee= date('Y-m-d H:i:s');
 $log = 'tlog.txt';
 file_put_contents($log,$datee.$var, FILE_APPEND | LOCK_EX);
 echo "saved $log..";
-$jdfile='d.txt';
+$jdfile='data.txt';
 file_put_contents($jdfile, $jnew);
 echo "saved $jdfile ..";
 echo "<hr>$datee";
