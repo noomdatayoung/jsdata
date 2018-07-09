7dd2253183cff3be060429c8ff731d7e5e82eee0
Yanyong Kunok <yanyong.kunok@gmail.com>
2018-07-09 22:22:22 +0700

    gen  file
    gen file from raw to array data

---
+++ gen.php
@@ -0,0 +1,40 @@
+<?
+$datee= date('Y-m-d H:i:s');
+echo "<hr>$datee";
+$file ='data.txt';
+$da=file_get_contents($file,true);
+$p = explode("\n", $da);
+for ($j=0;$j<=count($p);$j++){
+	if (strlen($p[$j])>0) $pp[]=$p[$j];	
+}
+$cp= count($p);
+$cpl=$cp-1;
+$jnew="data=[";
+for ($i=0;$i<=$j;$i++){
+//if (strlen($p[$i])>0) $ap.=$p[$i]."\r\n";
+//if (strlen($po[$i])>0) $up.=$po[$i]."\r\n";
+$jst="'";
+if ($i<$j-2) $jsend="',"; else $jsend="'";
+
+if (strlen($p[$i])>0) $daa=$p[$i];
+
+$daa=str_replace("\r","",$daa);
+$daa=str_replace("'","\'",$daa);
+$daa=str_replace('"','\"',$daa);
+$td=trim(daa);
+$ld=strlen($p[$i]);
+	if ($ld>0) $jnew.=$jst.$daa.$jsend;	
+}
+$jnew.="]";
+
+$var=" generate to js file .. $cpl #recs".PHP_EOL;
+$datee= date('Y-m-d H:i:s');
+$log = 'tlog.txt';
+file_put_contents($log,$datee.$var, FILE_APPEND | LOCK_EX);
+echo "saved $log..";
+$jdfile='d.txt';
+file_put_contents($jdfile, $jnew);
+echo "saved $jdfile ..";
+echo "<hr>$datee";
+exit;
+?>
